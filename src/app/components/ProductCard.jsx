58679e80c2beefc56625913f29b8674105f5d604
Alex-Aime <acheraime@usf.edu>
2025-11-02 19:38:50 -0500

    added a fuction where the product card displays the product name price and stock status

--- src/app/components/ProductCard.jsx
+++ src/app/components/ProductCard.jsx
@@ -1,1 +1,14 @@
 'use client';
+export default function ProductCard({product, inCart, onAdd}) {
+    const OutOfStock = product.stock <= 0
+
+    return (
+        <div>
+            <h3>{product.name}</h3>
+            <p>{product.price}</p>
+            <p className={`stock-status-sm mb3 ${OutOfStock ? "stock-status-red-600" : "stock-status-green-600"}`}>
+                {OutOfStock ? "Out of Stock": `${product.stock} in stock`}
+            </p>
+        </div>
+    )
+}
