a5f9a28c412b987499205cf856190e2aee36056b
Alex-Aime <acheraime@usf.edu>
2025-11-02 20:18:38 -0500

    made price ranges for my price filter

--- src/app/components/PriceFilter.jsx
+++ src/app/components/PriceFilter.jsx
@@ -1,1 +1,13 @@
 'use client';
+
+export default function PriceFilter({value, onChange}) {
+    const priceRanges = [
+        "All",
+        "0-51",
+        "52-156",
+        "157-268",
+        "269-501",
+        "502-1001",
+        "1002-2103"
+    ]
+}
