28bdce351124801a915fb9badc9a95ef9824b280
Alex-Aime <acheraime@usf.edu>
2025-11-02 20:45:33 -0500

    finsied the return on the cart summary function

--- src/app/components/CartSummary.jsx
+++ src/app/components/CartSummary.jsx
@@ -10,6 +10,26 @@ export default function CartSummary({cartItems, onDecrement, onReset}) {
             <h3>
                 Cart Summary
             </h3>
+            {items.length === 0 ? (<p>Your Cart is Empty</p>):
+            (
+                <>
+                    <ul>
+                        {items.map((item) => (
+                            <li key= {item.id}>
+                                <span>{item.name}</span>
+                                <div>
+                                    <span>${item.price}</span>
+                                    <button onClick={() => onDecrement(item.id)}>Remove</button>
+                                </div>
+                            </li>
+                        )
+                        )}
+                    </ul>
+                    <div>Total:${totalPrice}</div>
+                    <button onClick={onReset}>Reset</button>
+                </>
+            )}
         </div>
     )
-}
+
+};
