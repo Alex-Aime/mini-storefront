dd41ff1349b15853beb1b47bb8ac5d78c95e50a0
Alex-Aime <acheraime@usf.edu>
2025-11-02 20:35:15 -0500

    creating a function that takes the carts object items to an array so it can calculate the total ammout of cart items easier

--- src/app/components/CartSummary.jsx
+++ src/app/components/CartSummary.jsx
@@ -1,1 +1,15 @@
 'use client';
+
+export default function CartSummary({cartItems, onDecrement, onReset}) {
+    const items = Object.value(cartItems);
+
+    const totalPrice = items.reduce((sum, item) => sum + item.price, 0);
+
+    return(
+        <div>
+            <h3>
+                Cart Summary
+            </h3>
+        </div>
+    )
+}
