37c51a6f26e614e3a8526129fbf935add35ba1d3
Alex-Aime <acheraime@usf.edu>
2025-11-02 19:50:19 -0500

    added a button that that stays and dissapears based on if there is stock or not

--- src/app/components/ProductCard.jsx
+++ src/app/components/ProductCard.jsx
@@ -9,6 +9,12 @@ export default function ProductCard({product, inCart, onAdd}) {
             <p className={`stock-status-sm mb3 ${OutOfStock ? "stock-status-red-600" : "stock-status-green-600"}`}>
                 {OutOfStock ? "Out of Stock": `${product.stock} in stock`}
             </p>
+            <button className={`w-full py-2 mt auto rounded text-white transition
+                ${OutOfStock || inCart ? "bg-gray-400 cursor-not-allowed" : "bg-blue-600 hover:bg-blue-700"}`}
+                onClick={onAdd}
+                disabled={OutOfStock || inCart}>
+                {inCart ? "In Cart" : "Add to Cart"}
+            </button>
         </div>
     )
 }
