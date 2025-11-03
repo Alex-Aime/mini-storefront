18ab68fd950cd86c2613c3ad63dbd8ba53e4be3d
Alex-Aime <acheraime@usf.edu>
2025-11-02 20:12:54 -0500

    created my category filter

--- src/app/components/CategoryFilter.jsx
+++ src/app/components/CategoryFilter.jsx
@@ -1,1 +1,17 @@
 'use client';
+
+export default function CategoryFilter({categorgies, value, onChange}) {
+    return (
+        <div>
+            <label htmlFor="category">
+                Category
+            </label>
+            <select id="category" value={value} onChange={(e) => onChange(e.target.value)}>
+                {categorgies.map((cat) => (
+                    <option key={cat} value={cat}> 
+                        {cat}
+                    </option>))}
+            </select>
+        </div>
+    )
+}
